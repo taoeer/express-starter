3d1ea12a72b1ce235d58a89eb2fa0b06017cd0b5
huangtao <719012229@qq.com>
2018-12-01 10:00:05 +0800

    ==

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -4,13 +4,22 @@ const log4js = require('log4js');
 const Busboy = require('busboy');
 const logger = log4js.getLogger('uploadController');
 
-module.exports = function (req, res) {
+module.exports = function (req, res, next) {
   var busboy = new Busboy({ headers: req.headers });
-  logger.info(req.headers);
   busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-    logger.info('upload files ', fieldname, filename, encoding, mimetype)
-    var saveTo = path.join('upload', path.basename(filename));
-    file.pipe(fs.createWriteStream(saveTo));
+    var saveTo = path.join(__dirname, '../upload', path.basename(filename));
+    var dir = path.dirname(saveTo);
+    try {
+      fs.mkdirSync(dir)
+    } catch (e) {
+    }
+    const writeStream = fs.createWriteStream(saveTo);
+    writeStream.on('error', (e) => {
+      console.log('headersSent', req.headersSent);
+      console.log('error:',e);
+      next(e);
+    });
+    file.pipe(writeStream);
     busboy.on('finish', function() {
       res.writeHead(200, { 'Connection': 'close' });
       res.end("That's all folks! " + path.posix.resolve(saveTo).replace(/\\|\//g, path.sep));
--- rotues/index.js
+++ rotues/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const uploadController = require('../controllers/uploadController');
+const errorController = require('../controllers/errorController');
 
 const router = express.Router();
 
 router.post('/upload', uploadController);
 
-module.exports = router
+router.get('/error', errorController);
+
+module.exports = router;
